1f8e7ab41406525139de6bc46769e38a74256b2f
microsanya <microsanya@vk.com>
2024-08-04 15:27:19 +0700

    All program

---
+++ app/src/main/java/com/example/tomskforeveryone/LogInActivity.kt
@@ -0,0 +1,32 @@
+package com.example.tomskforeveryone
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    /** Called when the user clicks the Send button  */
+    fun sendMessage(view: View?) {
+        val intent = Intent(
+            this,
+            DisplayMessageActivity::class.java
+        )
+    }
+
+}
